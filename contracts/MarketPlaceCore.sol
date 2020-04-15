e88dce8069c1d4395221d76bb35129f65d8ca3a4
haidang91 <haiduc.dang@avenga.com>
2020-04-15 17:43:54 +0200

    contract inheritance

--- contracts/MarketPlaceCore.sol
+++ contracts/MarketPlaceCore.sol
@@ -2,8 +2,9 @@ pragma experimental ABIEncoderV2;
 pragma solidity ^0.5.12;
 
 import "./AccessControl.sol";
+import "./MarketPlaceBase.sol";
 
-contract MarketPlaceCore is AccessControl {
+contract MarketPlaceCore is AccessControl, MarketplaceBase {
 
   constructor(address _ethereumDIDRegistry, address _claimRegistry) public {
       registry = EthereumDIDRegistryInterface(_ethereumDIDRegistry); 
